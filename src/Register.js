dc1fd8fcc89a738ebd7fdb417f7a1143bba3a5e4
Loc Doan <u1438103@utah.edu>
2025-03-22 13:46:38 -0600

    Register page added

---
+++ src/Register.js
@@ -0,0 +1,83 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+const Register = () => {
+    const [formData, setFormData] = useState({
+        username: '',
+        email: '',
+        password: '',
+    });
+
+    const [error, setError] = useState('');
+    const [successMessage, setSuccessMessage] = useState('');
+    const navigate = useNavigate();
+
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setError('');
+        setSuccessMessage('');
+        try {
+            const response = await axios.post(`${process.env.REACT_APP_API_URL}/register`, formData);
+            console.log('Registration successful:', response.data);
+            setSuccessMessage('Registration successful!');
+            navigate('/login');
+        } catch (error) {
+            if (error.response && error.response.data && error.response.data.error){
+                setError(error.response.data.error)
+            } else {
+                setError('There was an error registering the user. Please try again.');
+            }
+        }
+    };
+
+    return (
+        <div>
+            <h2>Register</h2>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>Username:</label>
+                    <input
+                        type="text"
+                        name="username"
+                        value={formData.username}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+                <div>
+                    <label>Email:</label>
+                    <input
+                        type="email"
+                        name="email"
+                        value={formData.email}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+                <div>
+                    <label>Password:</label>
+                    <input
+                        type="password"
+                        name="password"
+                        value={formData.password}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+                {error && <p style={{ color: 'red' }}>{error}</p>}
+                {successMessage && <p style={{color: 'green'}}>{successMessage}</p>}
+                <button type="submit">Register</button>
+            </form>
+        </div>
+    );
+};
+
+export default Register;
